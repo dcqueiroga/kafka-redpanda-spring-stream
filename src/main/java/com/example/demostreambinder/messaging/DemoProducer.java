5f34afc096ad8a4139cea39dd1612555a3df1c3c
Daniel Queiroga <daniel.queiroga@radixeng.com.br>
2021-10-29 15:39:19 -0300

    Add consumer-group and partitions to allow multiple consumers

--- src/main/java/com/example/demostreambinder/messaging/DemoProducer.java
+++ src/main/java/com/example/demostreambinder/messaging/DemoProducer.java
@@ -2,6 +2,7 @@ package com.example.demostreambinder.messaging;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,16 +16,21 @@ public class DemoProducer {
 
     private final DemoBinder demoBinder;
 
+    @Value("${server.port:8080}")
+    private int port;
+
     private int counter;
 
     @Scheduled(initialDelay = 5_000, fixedDelay = 5_000)
     public void producer() {
-        IntStream.range(0, 10)
-            .forEach(value -> {
-                var message = String.format("Message test of %s - %s", counter, value);
-                demoBinder.demoOut().send(MessageBuilder.withPayload(message).build());
-                log.info("Message produced: {}", message);
-            });
-        counter++;
+        if (port == 8080) {
+            IntStream.range(0, 10)
+                    .forEach(value -> {
+                        var message = String.format("Message test of %s - %s", counter, value);
+                        demoBinder.demoOut().send(MessageBuilder.withPayload(message).build());
+                        log.info("Message produced: {}", message);
+                    });
+            counter++;
+        }
     }
 }
